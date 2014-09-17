5be3ef520cdf8d15d0d4b9af7856dd6389308fd9
wchaves <wilberth.chaves@gmail.com>
2014-09-17 09:57:12 -0600

    commit

--- Progra l Estructuras/main.cpp
+++ Progra l Estructuras/main.cpp
@@ -5,7 +5,20 @@
 #include "Constants.h"
 #include "LevyCCurve.h"
 
+
 using namespace std;
+
+int distancex = 45;
+int distancey = 45;
+
+void printLine(int x, int y, int x2, int y2, bool rumbo){
+
+    circle(x,y, 10);
+    int x2 = x + DISTANCE / 2;
+    int y2 = y + DISTANCE / 2;
+    setcolor(COLOR(12,100,0));
+    line (x, y, x2, y2);
+}
 
 
 int main()
@@ -20,22 +33,29 @@ int main()
     initwindow (1200,900);
     int x = 1200/2;
     int y = 900/2;
-    /*circle(x,y, 10);
-    int x2 = x + 45;
-    int y2 = y + 45;
-    setcolor(COLOR(12,100,0));
-    line (x, y, x2, y2);
+
 
     x = x2;
     y = y2;
 
     setcolor(COLOR(255,255,255));
-    x2 = x + 45;
-    y2 = y - 45;
+    x2 = x + (-45) / 2;
+    y2 = y + (-45) / 2;
     circle(x,y, 10);
-    line (x, y, x2, y2);*/
+    line (x, y, x2, y2);
 
-    lineto(x,y);
+
+    x = x2;
+    y = y2;
+
+    setcolor(COLOR(0,128,255));
+    x2 = x + (-45) / 2;
+    y2 = y + (45) / 2;
+    //y2 = 700;
+    circle(x2,y2, 10);
+    line (x, y, x2, y2);
+
+    //lineto(x,y);
 
     //line(0,7,80,90);
     getch();
